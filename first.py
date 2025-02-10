dd4c5a99c3f089aad93c22446c497705999f30a9
Tathagata <tathagatasau321@gmail.com>
2025-02-11 00:22:23 +0530

    Now heading for entrybox and grids

--- first.py
+++ first.py
@@ -1,8 +1,19 @@
 from tkinter import *
 window = Tk()
 window.title("Hello Stranger!")
-window. geometry("600x1080")
+window. geometry("600x890")
 window.configure(bg="light blue")
-inp = Label(window, text= "Hello World" )
-inp.pack()
+inp = Label(window, text= "Hello World", bg = "blue", fg = "white" )
+inp1 = Label(window, text= "Hello mice", bg = "black", fg = "white" )
+inp2 = Label(window, text= "Hello cat", bg = "cyan", fg = "red" )
+
+def log_entry():
+    print("logged in")
+
+button = Button(window, text = "LogIn", command= log_entry, bg= "orange", fg = "white")
+
+inp.pack(side = TOP, fill = X, expand = False)
+inp1.pack(side = LEFT, fill = Y, expand = False)
+inp2.pack(side = RIGHT, fill = Y, expand = False)
+button.pack()
 window.mainloop()
